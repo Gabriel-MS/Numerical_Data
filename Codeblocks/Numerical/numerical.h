b9d9f2eeecf79a04e0f6908d0469eb3229fda100
Gabriel-MS <Gabriel-MS@users.noreply.github.com>
2016-09-08 17:22:00 -0300

    Update numerical.h

--- Codeblocks/Numerical/numerical.h
+++ Codeblocks/Numerical/numerical.h
@@ -1,60 +1,174 @@
-#include "spline.h"
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include "nrutil.h" //using numerical recipes c++
+#include "interpolation_util.h"
 
-template<class Function>
-double deriv1(const Function& f, double x)
+//Finite difference method to calculate approximate first derivative
+//x and y vectors must have the same size
+//Outputs first derivatives in vector
+double fin_diff_1_vec(double x[], double y[])
 {
-double dx=1e-8*(1.0+fabs(x));
-double x0=x-dx;
-double x2=x+dx;
-return (f(x2)-f(x0)) / (2.0*dx);
+  int size, i;
+
+  size = x.size();
+
+  double y1[size]; //Vector to output first derivatives
+  
+  y1[0] = y[0]/x[0]; //Wrong, fix
+  
+  for(i=1; i<size; i++)
+  {
+    y1[i] = (y[i]-y[i-1])/(x[i]-x[i-1]);
+  }
+
+return y1;
 }
-template<class Function>
-double deriv2(const Function& f, double x)
+
+
+//Finite difference method to calculate approximate first derivative
+//x and y vectors must have the same size
+//x MUST have equal spacing between neighbour characters
+//Outputs first derivatives in vector
+double fin_diff_2_vec(double x[], double y[])
 {
-double dx=1e-6*(1.0+fabs(x));
-double x0=x-dx;
-double x2=x+dx;
-return (f(x0)-2.0*f(x)+f(x2)) / (dx*dx);
+  int size, i;
+
+  size = x.size();
+
+  double y2[size]; //Vector to output first derivatives
+  
+  y2[0] = (y[1]- 2*y[0])/(pow(x[1]-x[0]),2); //Wrong, fix
+  
+  for(i=1; i<size; i++)
+  {
+    y2[i] = (y[i+1]-2*y[i]+y[i-1])/(pow(x[i]-x[i-1]),2);
+  }
+
+return y2;
 }
-int main(int argc, char** argv)
+
+
+//Cubic spline, outputs 2nd derivative to use in interpolation function
+//end1 and endn are the specified first derivatives boundaries 
+//at the begin and end respectively
+/*                     UNDER DEVELOPMENT
+double cubic_spline(double x[], double y[], int n, double end1, double endn, double y2[])
 {
-if(argc<1) {
-printf("usage: %s <>\n", argv[0]);
-exit(EXIT_FAILURE);
+  int i,k;
+  double p,qn,sig,un,*u;
+  
+  u=vector(1,n-1);
+  if (yp1 > 0.99e30) y2[1]=u[1]=0.0;
+    else 
+    {
+    y2[1] = -0.5;
+    u[1]=(3.0/(x[2]-x[1]))*((y[2]-y[1])/(x[2]-x[1])-yp1);
+    }
+  
+  
+  for (i=2;i<=n-1;i++) 
+  {
+  sig=(x[i]-x[i-1])/(x[i+1]-x[i-1]);
+  p=sig*y2[i-1]+2.0;
+  y2[i]=(sig-1.0)/p;
+  u[i]=(y[i+1]-y[i])/(x[i+1]-x[i]) - (y[i]-y[i-1])/(x[i]-x[i-1]);
+  u[i]=(6.0*u[i]/(x[i+1]-x[i-1])-sig*u[i-1])/p;
+  }
+  
+  if (ypn > 0.99e30) qn=un=0.0;
+    else 
+    {
+    qn=0.5;
+    un=(3.0/(x[n]-x[n-1]))*(ypn-(y[n]-y[n-1])/(x[n]-x[n-1]));
+    }
+    
+  y2[n]=(un-qn*u[n-1])/(qn*y2[n-1]+1.0);
+  
+  for (k=n-1;k>=1;k--)  y2[k]=y2[k]*y2[k+1]+u[k];
+  
+  free_vector(u,1,n-1);
 }
-std::vector<double> X(5), Y(5);
-X[0]=0.1;
-X[1]=0.4;
-X[2]=1.2;
-X[3]=1.8;
-X[4]=2.0;
-Y[0]=0.1;
-Y[1]=0.7;
-Y[2]=0.6;
-Y[3]=1.1;
-Y[4]=0.9;
-tk::spline s;
-// set_boundary() is optional and if omitted, natural boundary condition,
-// f''(a)=f''(b)=0, will be used
-// note, if natural boundary conditions are not used then extrapolation
-// will be a quadratic function, unless the last argument is set to true,
-// which forces linear extrapolation, but this will violate second order
-// differentiability at the endpoint
-s.set_boundary(tk::spline::second_deriv, 0.0,
-tk::spline::first_deriv, -2.0, false);
-s.set_points(X,Y);
-for(size_t i=0; i<X.size(); i++) {
-printf("%f %f\n", X[i], Y[i]);
+*/
+
+
+//Cubic spline interpolation, interpolates single values in cubic splines
+double cspline(double X[], double Y[], double x)
+{
+  int size, i;
+  
+  tk::spline s;
+  s.set_points(X,Y);
+
+  size = X.size();
+
+  double y1; //Output first derivatives
+
+  y1 = s(1,x);
+
+return y1;
 }
-printf("\n");
-for(int i=-50; i<250; i++) {
-double x=0.01*i;
-printf("%f %f %f %f %f\n", x, s(x),
-s.deriv(1,x), s.deriv(2,x), s.deriv(3,x));
-// checking analytic derivatives and finite differences are close
-assert(fabs(s.deriv(1,x)-deriv1(s,x)) < 1e-8);
-assert(fabs(s.deriv(2,x)-deriv2(s,x)) < 1e-8);
+
+
+//Cubic spline interpolation, interpolates vectors in cubic splines
+double cspline_vec(double X[], double Y[], double x[])
+{
+  int size, i;
+  
+  tk::spline s;
+  s.set_points(X,Y);
+
+  size = X.size();
+
+  double y1[size]; //Vector to output first derivatives
+
+  for(i=1; i<size; i++)
+  {
+    y1[i] = s(1,x[i]);
+  }
+
+return y1;
 }
-printf("\n");
-return EXIT_SUCCESS;
+
+
+//Cubic spline derivative, outputs values of 1st derivatives in cubic splines
+double cspline_deriv1(double X[], double Y[], double x)
+{
+  int size, i;
+  
+  tk::spline s;
+  s.set_points(X,Y);
+
+  size = X.size();
+
+  double y1[size]; //Vector to output first derivatives
+
+  for(i=1; i<size; i++)
+  {
+    y1[i] = s.deriv(1,x);
+  }
+
+return y1;
+}
+
+
+//Cubic spline derivative, outputs vectors of 1st derivatives in cubic splines
+double cspline_deriv1_vec(double X[], double Y[], double x[])
+{
+  int size, i;
+  
+  tk::spline s;
+  s.set_points(X,Y);
+
+  size = X.size();
+
+  double y1[size]; //Vector to output first derivatives
+
+  for(i=1; i<size; i++)
+  {
+    y1[i] = s.deriv(1,x[i]);
+  }
+
+return y1;
 }
